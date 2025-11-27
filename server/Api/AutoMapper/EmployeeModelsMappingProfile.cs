cfbbf2ece8a79720fa7f906222f7637e05d51beb
Agatha Sates <satesagatha@gmail.com>
2025-11-27 05:07:54 -0300

    feat(api): add AutoMapper profile for employee models

---
+++ server/Api/AutoMapper/EmployeeModelsMappingProfile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Immutable;
+using AutoMapper;
+using OblivionDrive.Api.Models.EmployeeModule;
+using OblivionDrive.Application.EmployeeModule.Commands;
+using OblivionDrive.Application.EmployeeModule.DTOs;
+using OblivionDrive.Application.EmployeeModule.Querys;
+
+namespace OblivionDrive.Api.AutoMapper;
+
+public class EmployeeModelsMappingProfile : Profile
+{
+    public EmployeeModelsMappingProfile() 
+    {
+        CreateMap<RegisterEmployeeRequest, RegisterEmployeeCommand>();
+        CreateMap<EmployeeDTO, RegisterEmployeeResponse>();
+
+        CreateMap<UpdatedEmployeeDTO, UpdateEmployeeByCompanyResponse>();
+        CreateMap<UpdatedEmployeeDTO, UpdateOwnEmployeeResponse>();
+
+        CreateMap<DetailEmployeeDTO, GetEmployeeByCompanyResponse>();
+
+        CreateMap<EmployeesResult, GetAllEmployeesForCompanyResponse>()
+            .ConvertUsing((src, dest, ctx) => new GetAllEmployeesForCompanyResponse(
+                src.Employees.Count,
+                src?.Employees?
+                    .Select(e => ctx.Mapper.Map<DetailEmployeeDTO>(e))
+                    .ToImmutableList()
+                ?? ImmutableList<DetailEmployeeDTO>.Empty
+            ));
+    }
+}
