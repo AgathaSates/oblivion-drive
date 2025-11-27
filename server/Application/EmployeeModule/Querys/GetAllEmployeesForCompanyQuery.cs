d7c7eab8f5036222ce08d47c2c56f0be8d00bd65
Agatha Sates <satesagatha@gmail.com>
2025-11-27 05:02:42 -0300

    feat(application.employee): add employee queries

---
+++ server/Application/EmployeeModule/Querys/GetAllEmployeesForCompanyQuery.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Immutable;
+using FluentResults;
+using MediatR;
+using OblivionDrive.Application.EmployeeModule.DTOs;
+
+namespace OblivionDrive.Application.EmployeeModule.Querys;
+public record GetAllEmployeesForCompanyQuery(int? Quantity)
+    : IRequest<Result<EmployeesResult>>;
+
+public record EmployeesResult(ImmutableList<DetailEmployeeDTO> Employees);
---
+++ server/Application/EmployeeModule/Querys/GetEmployeeByIdForCompanyQuery.cs
@@ -0,0 +1,6 @@
+﻿using FluentResults;
+using MediatR;
+using OblivionDrive.Application.EmployeeModule.DTOs;
+
+namespace OblivionDrive.Application.EmployeeModule.Querys;
+public record GetEmployeeByIdForCompanyQuery(Guid EmployeeId) : IRequest<Result<DetailEmployeeDTO>>;
