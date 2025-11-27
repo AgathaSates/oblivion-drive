5b2532deed04a41a037151aba49eb3fa292e72e0
Agatha Sates <satesagatha@gmail.com>
2025-11-27 04:54:33 -0300

    feat(infrastructure): add employee infrastructure layer

---
+++ server/Infrastructure/EmployeeModule/EmployeeOrmMapper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OblivionDrive.Domain.EmployeeModule;
+
+namespace OblivionDrive.Infrastructure.Orm.EmployeeModule;
+public class EmployeeOrmMApper : IEntityTypeConfiguration<Employee>
+{
+    public void Configure(EntityTypeBuilder<Employee> builder)
+    {
+        builder.HasKey(e => e.Id);
+
+        builder.Property(e => e.CompanyId)
+            .IsRequired();
+
+        // Relação com Identity User
+        builder.Property(e => e.IdentityUserId)
+            .IsRequired();
+
+        builder.HasOne(e => e.IdentityUser)
+            .WithMany()
+            .HasForeignKey(e => e.IdentityUserId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.Property(e => e.Name)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(e => e.HireDate)
+            .IsRequired();
+
+        builder.Property(e => e.Salary)
+            .IsRequired()
+            .HasPrecision(18, 2);
+    }
+}
---
+++ server/Infrastructure/EmployeeModule/EmployeeOrmRepository.cs
@@ -0,0 +1,13 @@
+﻿using OblivionDrive.Domain.EmployeeModule;
+using OblivionDrive.Infrastructure.Orm.Shared;
+
+namespace OblivionDrive.Infrastructure.Orm.EmployeeModule;
+public class EmployeeOrmRepository(OblivionDriveDbContext context) : BaseRepository<Employee>(context), IRepositoryEmployee
+{
+    public Task<Employee> UpdateOwnProfileNameAsync(Employee employee, string newName)
+    {
+        employee!.UpdateOwnProfileName(newName);
+
+        return Task.FromResult(employee);
+    }
+}
