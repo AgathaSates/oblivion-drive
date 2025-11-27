73814b076498a9ab43c6df557c6a8f7156cf6251
Agatha Sates <satesagatha@gmail.com>
2025-11-27 04:52:33 -0300

    feat(domain): add employee domain model and repository interface

---
+++ server/Domain/EmployeeModule/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using OblivionDrive.Domain.AuthenticationModule;
+using OblivionDrive.Domain.Shared;
+
+namespace OblivionDrive.Domain.EmployeeModule;
+public class Employee : TenantEntity<Employee>
+{
+    public Guid IdentityUserId { get; private set; }
+    public User IdentityUser { get; private set; } = null!;
+
+    public string Name { get; private set; }
+    public DateOnly HireDate { get; private set; }
+    public decimal Salary { get; private set; }
+
+    [ExcludeFromCodeCoverage]
+    private Employee() { }
+
+    public Employee(Guid EmployeeId,Guid companyId,User identityUser, Guid identityUserId,
+        string name, DateOnly hireDate, decimal salary)
+    {
+        CompanyId = companyId;
+        IdentityUser = identityUser;
+        IdentityUserId = identityUserId;
+
+        Id = EmployeeId;
+        Name = name;
+        HireDate = hireDate;
+        Salary = salary;
+    }
+
+    public override void Update(Employee updatedEntity)
+    {
+        Name = updatedEntity.Name;
+        HireDate = updatedEntity.HireDate;
+        Salary = updatedEntity.Salary;
+    }
+
+    public void UpdateOwnProfileName(string name)
+    {
+        Name = name;
+    }
+}
---
+++ server/Domain/EmployeeModule/IRepositoryEmployee.cs
@@ -0,0 +1,7 @@
+﻿using OblivionDrive.Domain.Shared;
+
+namespace OblivionDrive.Domain.EmployeeModule;
+public interface IRepositoryEmployee : IRepository<Employee> 
+{
+    Task<Employee> UpdateOwnProfileNameAsync(Employee employee, string newName);
+}
