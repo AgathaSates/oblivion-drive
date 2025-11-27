bd2aab9d3e14c8377ac324348a518927db9e7252
Agatha Sates <satesagatha@gmail.com>
2025-11-27 05:00:18 -0300

    feat(mapping): add AutoMapper profile for employee module

---
+++ server/Application/AutoMapper/EmployeeMappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using OblivionDrive.Application.EmployeeModule.DTOs;
+using OblivionDrive.Application.EmployeeModule.Querys;
+using OblivionDrive.Domain.EmployeeModule;
+
+namespace OblivionDrive.Application.AutoMapper;
+internal class EmployeeMappingProfile : Profile
+{
+    public EmployeeMappingProfile()
+    {
+        CreateMap<Employee, EmployeeDTO>()
+            .ForMember(dest => dest.CreatedSuccessfully,
+                opt => opt.MapFrom(_ => true))
+            .ForMember(dest => dest.UserName,
+                opt => opt.MapFrom(src => src.IdentityUser.UserName ?? string.Empty));
+
+        CreateMap<Employee, UpdatedEmployeeDTO>()
+            .ForMember(dest => dest.UpdatedSuccessfully,
+                opt => opt.MapFrom(_ => true));
+
+        CreateMap<Employee, DetailEmployeeDTO>();
+    }
+}
