50e9d218c8331d11d3d1d62a6394c61fd88d825d
Agatha Sates <satesagatha@gmail.com>
2025-11-27 04:57:49 -0300

    feat(application): add name formatter helper

---
+++ server/Application/Shared/NameFormatter.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+
+namespace OblivionDrive.Application.Shared;
+public static class NameFormatter
+{
+    private static readonly CultureInfo DefaultCulture = new("pt-BR");
+
+    private static readonly HashSet<string> LowercaseParticles =
+    [
+        "da", "de", "do", "das", "dos", "e"
+    ];
+
+    public static string FormatName(string rawName)
+    {
+        if (string.IsNullOrWhiteSpace(rawName))
+            return string.Empty;
+
+        string[] words = rawName
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(word => word.ToLower(DefaultCulture))
+            .ToArray();
+
+        for (int index = 0; index < words.Length; index++)
+        {
+            string currentWord = words[index];
+
+            if (index > 0 && LowercaseParticles.Contains(currentWord))
+            {
+                words[index] = currentWord;
+                continue;
+            }
+
+            char firstChar = char.ToUpper(currentWord[0], DefaultCulture);
+            string remainingText = currentWord.Length > 1
+                ? currentWord[1..]
+                : string.Empty;
+
+            words[index] = firstChar + remainingText;
+        }
+
+        return string.Join(' ', words);
+    }
+}
