7fe00f8710b0cac3bcb1273e239409ee71411d89
Agatha Sates <satesagatha@gmail.com>
2025-11-27 22:50:39 -0300

    feat(mapping): add AutoMapper profile for FuelPrice module

---
+++ server/Application/AutoMapper/FuelPriceConfigurationMappingProfile .cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using OblivionDrive.Application.FuelPriceConfigurationModule.DTOs;
+using OblivionDrive.Domain.FuelPriceConfigurationModule;
+
+namespace OblivionDrive.Application.AutoMapper;
+public class FuelPriceConfigurationMappingProfile : Profile
+{
+    public FuelPriceConfigurationMappingProfile()
+    {
+        CreateMap<FuelPriceConfiguration, FuelPriceConfigurationDto>()
+            .ConstructUsing(f => new FuelPriceConfigurationDto(
+                f.Gasoline,
+                f.Gas,
+                f.Diesel,
+                f.Alcohol,
+                f.LastUpdate));
+    }
+}
