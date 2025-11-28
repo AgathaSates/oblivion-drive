ba0481ca050966fb8e6c20d2c81602d3e9d652f3
Agatha Sates <satesagatha@gmail.com>
2025-11-27 22:51:59 -0300

    feat(application.fuelprice): add FuelPrice queries

---
+++ server/Application/FuelPriceConfigurationModule/Querys/GetFuelPriceConfigurationQuery.cs
@@ -0,0 +1,6 @@
+﻿using FluentResults;
+using MediatR;
+using OblivionDrive.Application.FuelPriceConfigurationModule.DTOs;
+
+namespace OblivionDrive.Application.FuelPriceConfigurationModule.Querys;
+public record GetFuelPriceConfigurationQuery() : IRequest<Result<FuelPriceConfigurationDto>>;
