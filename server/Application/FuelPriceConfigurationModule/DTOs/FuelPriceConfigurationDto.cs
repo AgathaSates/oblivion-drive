6a7105112dfe0c239e5b43dfba342388306e994e
Agatha Sates <satesagatha@gmail.com>
2025-11-27 22:51:02 -0300

    feat(application.employee): add FuelPrice DTOs

---
+++ server/Application/FuelPriceConfigurationModule/DTOs/FuelPriceConfigurationDto.cs
@@ -0,0 +1,8 @@
+﻿
+namespace OblivionDrive.Application.FuelPriceConfigurationModule.DTOs;
+public record FuelPriceConfigurationDto(
+    decimal Gasoline,
+    decimal Gas,
+    decimal Diesel,
+    decimal Alcohol,
+    DateOnly LastUpdate);
