4d04ca20aca3e681a18e5965a64a745fd575da69
Agatha Sates <satesagatha@gmail.com>
2025-11-27 22:51:15 -0300

    feat(application.employee): add FuelPrice commands

---
+++ server/Application/FuelPriceConfigurationModule/Commands/UpdateFuelPriceConfigurationCommand.cs
@@ -0,0 +1,12 @@
+﻿using FluentResults;
+using MediatR;
+using OblivionDrive.Application.FuelPriceConfigurationModule.DTOs;
+
+namespace OblivionDrive.Application.FuelPriceConfigurationModule.Commands;
+
+public record UpdateFuelPriceConfigurationCommand(
+    decimal Gasoline,
+    decimal Gas,
+    decimal Diesel,
+    decimal Alcohol) : IRequest<Result<FuelPriceConfigurationDto>>;
+
