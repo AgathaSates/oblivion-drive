5a7f837b72ea0fc114fded8188080a6fd7c25372
Agatha Sates <satesagatha@gmail.com>
2025-11-29 03:49:33 -0300

    feat(application): add Service queries

---
+++ server/Application/ServicesModule/Querys/GetAllServicesQuery.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Immutable;
+using FluentResults;
+using MediatR;
+using OblivionDrive.Application.ServicesModule.DTOs;
+
+namespace OblivionDrive.Application.ServicesModule.Querys;
+public record GetAllServicesQuery(int? Quantity) : IRequest<Result<ServicesResult>>;
+
+public record ServicesResult(ImmutableList<DetailServiceDTO> Services);
---
+++ server/Application/ServicesModule/Querys/GetServiceByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using FluentResults;
+using MediatR;
+using OblivionDrive.Application.ServicesModule.DTOs;
+
+namespace OblivionDrive.Application.ServicesModule.Querys;
+public record GetServiceByIdQuery(Guid ServiceId) : IRequest<Result<DetailServiceDTO>>;
