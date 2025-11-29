847f7d4c63685b70f4c5c853dcb25c988bbf44ec
Agatha Sates <satesagatha@gmail.com>
2025-11-29 03:48:20 -0300

    feat(mapping): add AutoMapper profile for Servicemodule

---
+++ server/Application/AutoMapper/ServiceMappingProfile .cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using OblivionDrive.Application.ServicesModule.DTOs;
+using OblivionDrive.Domain.ServicesModule;
+
+namespace OblivionDrive.Application.AutoMapper;
+public class ServiceMappingProfile : Profile
+{
+    public ServiceMappingProfile()
+    {
+        CreateMap<Service, ServiceDTO>()
+            .ConstructUsing(service => new ServiceDTO(
+                true,
+                service.Name,
+                service.Price,
+                service.ChargeType));
+
+        CreateMap<Service, UpdatedServiceDTO>()
+            .ConstructUsing(service => new UpdatedServiceDTO(
+                true,
+                service.Name,
+                service.Price,
+                service.ChargeType));
+
+        CreateMap<Service, DetailServiceDTO>();
+    }
+}
