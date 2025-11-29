bb0e408d48723b54da7d7bb2967e57f12677908f
Agatha Sates <satesagatha@gmail.com>
2025-11-29 03:50:49 -0300

    feat(api): add AutoMapper profile for Service models

---
+++ server/Api/AutoMapper/ServicesModelsMappingProfile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Immutable;
+using AutoMapper;
+using OblivionDrive.Api.Models.ServicesModule;
+using OblivionDrive.Application.ServicesModule.Commands;
+using OblivionDrive.Application.ServicesModule.DTOs;
+using OblivionDrive.Application.ServicesModule.Querys;
+
+namespace OblivionDrive.Api.AutoMapper;
+
+public class ServicesModelsMappingProfile : Profile
+{
+    public ServicesModelsMappingProfile()
+    {
+        CreateMap<RegisterServiceRequest, RegisterServiceCommand>();
+        CreateMap<ServiceDTO, RegisterServiceResponse>();
+        CreateMap<(Guid, UpdateServiceRequest), UpdateServiceCommand>()
+            .ConvertUsing(src => new UpdateServiceCommand(
+                src.Item1,
+                src.Item2.Name,
+                src.Item2.Price,
+                src.Item2.ChargeType
+                ));
+
+        CreateMap<UpdatedServiceDTO, UpdateServiceResponse>();
+        CreateMap<DetailServiceDTO, GetServiceByIdResponse>();
+        CreateMap<ServicesResult, GetAllServicesResponse>()
+            .ConvertUsing((src, dest, ctx) => new GetAllServicesResponse(
+                src.Services.Count,
+                src?.Services?
+                    .Select(s => ctx.Mapper.Map<DetailServiceDTO>(s))
+                    .ToImmutableList() ?? ImmutableList<DetailServiceDTO>.Empty
+            ));
+    }
+}
