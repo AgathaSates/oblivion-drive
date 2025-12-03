5aaec99253d7056329d42fb32b97ba5fa712641d
Agatha Sates <satesagatha@gmail.com>
2025-12-03 20:54:02 -0300

    feat(client): add carousel component

---
+++ client/src/app/components/home/sections/as-carousel/as-carousel.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'as-carousel',
+  imports: [],
+  templateUrl: './as-carousel.html',
+  styleUrl: './as-carousel.scss',
+})
+export class AsCarousel {}
