d31201a1e5ef8de2d33757c2845e8cc7a6e1e1d0
Agatha Sates <satesagatha@gmail.com>
2025-12-03 20:56:07 -0300

    chore(client): add app configuration

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,16 +1,59 @@
 import {
   ApplicationConfig,
+  inject,
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { CanActivateFn, provideRouter, Router, Routes } from '@angular/router';
+import { provideAuth } from './components/modules/auth/models/auth.provider';
+import { provideNotifications } from './components/shared/notification/notificacao.provider';
+import { take, map } from 'rxjs';
+import { AuthService } from './components/modules/auth/services/auth.service';
 
-import { routes } from './app.routes';
+const UnknownUserGuard: CanActivateFn = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  return authService.accessToken$.pipe(
+    take(1),
+    map((token) => (!token ? true : router.createUrlTree(['/inicio']))),
+  );
+};
+
+const AuthenticatedUserGuard: CanActivateFn = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  return authService.accessToken$.pipe(
+    take(1),
+    map((token) => (token ? true : router.createUrlTree(['/auth/login']))),
+  );
+};
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'auth/login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'inicio',
+    loadComponent: () => import('./components/home/pages/as-home/as-home').then((m) => m.AsHome),
+    canActivate: [AuthenticatedUserGuard],
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./components/modules/auth/auth.routes').then((m) => m.authRoutes),
+    canActivate: [UnknownUserGuard],
+  },
+];
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
+    provideNotifications(),
+    provideAuth(),
   ],
 };
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,9 +1,24 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { AsShellComponent } from './components/shared/shell/as-shell.component';
+import { MatIcon } from '@angular/material/icon';
+import { AuthService } from './components/modules/auth/services/auth.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AsShellComponent, RouterOutlet, MatIcon, AsyncPipe],
   templateUrl: './app.html',
 })
-export class App {}
+export class App {
+  protected readonly router = inject(Router);
+
+  private readonly authService = inject(AuthService);
+  protected readonly accessToken$ = this.authService.accessToken$;
+
+  public logout(): void {
+    this.authService.logout().subscribe(() => {
+      this.router.navigate(['/auth/login']);
+    });
+  }
+}
