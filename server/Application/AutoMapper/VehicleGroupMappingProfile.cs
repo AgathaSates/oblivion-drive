05a106072fb927d4d73fec52af3ef66719f291d7
Agatha Sates <satesagatha@gmail.com>
2025-12-04 03:45:19 -0300

    feat(mapping): add AutoMapper profile for VehicleGroup

---
+++ server/Application/AutoMapper/VehicleGroupMappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using OblivionDrive.Application.VehicleGroupModule.DTOs;
+using OblivionDrive.Domain.VehicleGroupModule;
+
+namespace OblivionDrive.Application.AutoMapper;
+public class VehicleGroupMappingProfile : Profile
+{
+    public VehicleGroupMappingProfile()
+    {
+        CreateMap<VehicleGroup, VehicleGroupDTO>()
+            .ConstructUsing(vehicleGroup => new VehicleGroupDTO(
+                true,
+                vehicleGroup.Name));
+
+        CreateMap<VehicleGroup, UpdatedVehicleGroupDTO>()
+            .ConstructUsing(vehicleGroup => new UpdatedVehicleGroupDTO(
+                true,
+                vehicleGroup.Name));
+
+        CreateMap<VehicleGroup, DetailVehicleGroupDTO>();
+    }
+}
