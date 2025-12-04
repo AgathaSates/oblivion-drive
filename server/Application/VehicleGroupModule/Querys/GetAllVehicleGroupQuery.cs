8c83b02f6f7c7ce2498da5c2a6a0e7fe02a1c177
Agatha Sates <satesagatha@gmail.com>
2025-12-04 03:46:10 -0300

    feat(application): add VehicleGroup queries

---
+++ server/Application/VehicleGroupModule/Querys/GetAllVehicleGroupQuery.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Immutable;
+using FluentResults;
+using MediatR;
+using OblivionDrive.Application.VehicleGroupModule.DTOs;
+
+namespace OblivionDrive.Application.VehicleGroupModule.Querys;
+public record GetAllVehicleGroupQuery(int? Quantity) : IRequest<Result<VehicleGroupResult>>;
+
+public record VehicleGroupResult(ImmutableList<DetailVehicleGroupDTO> VehicleGroups);
---
+++ server/Application/VehicleGroupModule/Querys/GetVehicleGroupByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using FluentResults;
+using MediatR;
+using OblivionDrive.Application.VehicleGroupModule.DTOs;
+
+namespace OblivionDrive.Application.VehicleGroupModule.Querys;
+public record GetVehicleGroupByIdQuery(Guid VehicleGroupId) : IRequest<Result<DetailVehicleGroupDTO>>;
