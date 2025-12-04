f98f0f5192c955b2d72e1e78fc2fe35a0de8911d
Agatha Sates <satesagatha@gmail.com>
2025-12-04 03:47:20 -0300

    feat(api): add AutoMapper profile for VehicleGroup models

---
+++ server/Api/AutoMapper/VehicleGroupsModelsMappingProfile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Immutable;
+using AutoMapper;
+using OblivionDrive.Api.Models.VehicleGroupModule;
+using OblivionDrive.Application.VehicleGroupModule.commands;
+using OblivionDrive.Application.VehicleGroupModule.DTOs;
+using OblivionDrive.Application.VehicleGroupModule.Querys;
+
+namespace OblivionDrive.Api.AutoMapper;
+
+public class VehicleGroupsModelsMappingProfile : Profile
+{
+    public VehicleGroupsModelsMappingProfile()
+    {
+        CreateMap<RegisterVehicleGroupRequest, RegisterVehicleGroupCommand>();
+        CreateMap<VehicleGroupDTO, RegisterVehicleGroupResponse>();
+        CreateMap<(Guid, UpdateVehicleGroupRequest), UpdateVehicleGroupCommand>()
+            .ConvertUsing(src => new UpdateVehicleGroupCommand(
+                src.Item1,
+                src.Item2.Name
+                ));
+        CreateMap<UpdatedVehicleGroupDTO, UpdateVehicleGroupResponse>();
+        CreateMap<DetailVehicleGroupDTO, GetVehicleGroupByIdResponse>();
+        CreateMap<VehicleGroupResult, GetAllVehicleGroupResponse>()
+            .ConvertUsing((src, dest, ctx) => new GetAllVehicleGroupResponse(
+                src.VehicleGroups.Count,
+                src?.VehicleGroups?
+                    .Select(vg => ctx.Mapper.Map<DetailVehicleGroupDTO>(vg))
+                    .ToImmutableList() ?? ImmutableList<DetailVehicleGroupDTO>.Empty
+            ));
+    }
+}
