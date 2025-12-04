7e8b6317ab5f4b707a79e00914f6732e2f57bacb
Agatha Sates <satesagatha@gmail.com>
2025-12-04 03:43:54 -0300

    feat(domain): add VehicleGroup domain model and repository interface

---
+++ server/Domain/VehicleGroupModule/IRepositoryVehicleGroup.cs
@@ -0,0 +1,8 @@
+﻿using OblivionDrive.Domain.Shared;
+
+namespace OblivionDrive.Domain.VehicleGroupModule;
+public interface IRepositoryVehicleGroup : IRepository<VehicleGroup>
+{
+    Task<bool> ExistsByNameAsync(string vehicleGroupName, Guid vehicleGroupIdToIgnore);
+    Task<bool> ExistsByNameAsync(string vehicleGroupName);
+}
---
+++ server/Domain/VehicleGroupModule/VehicleGroup.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using OblivionDrive.Domain.BillingPlanModule;
+using OblivionDrive.Domain.Shared;
+
+namespace OblivionDrive.Domain.VehicleGroupModule;
+
+public class VehicleGroup : TenantEntity<VehicleGroup>
+{
+    public string Name { get; private set; }
+    public ICollection<BillingPlan> BillingPlans { get; private set; } = new List<BillingPlan>();
+
+
+    [ExcludeFromCodeCoverage]
+    private VehicleGroup() { }
+
+    public VehicleGroup(string name, Guid companyId)
+    {
+        Id = Guid.NewGuid();
+        CompanyId = companyId;
+        Name = name;
+    }
+
+    public override void Update(VehicleGroup updatedEntity)
+    {
+        Name = updatedEntity.Name;
+    }
+}
