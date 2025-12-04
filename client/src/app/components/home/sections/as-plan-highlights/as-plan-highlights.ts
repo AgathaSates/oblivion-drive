c090ceb4f0727531aa1df4028dcfb81a24cb5650
Agatha Sates <satesagatha@gmail.com>
2025-12-03 21:02:21 -0300

    feat(client): add plans highlight component

---
+++ client/src/app/components/home/sections/as-plan-highlights/as-plan-highlights.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'as-plan-highlights',
+  imports: [MatIcon],
+  templateUrl: './as-plan-highlights.html',
+  styleUrl: './as-plan-highlights.scss',
+})
+export class AsPlanHighlights {}
