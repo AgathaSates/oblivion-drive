9ba04c0f5135386bf514e1e3b82fb98bbfbbd5c3
Agatha Sates <satesagatha@gmail.com>
2025-12-05 01:11:29 -0300

    feat(application): add BillingPlan queries

---
+++ server/Application/BillingPlanModule/Querys/GetAllBillingPlanQuery.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Immutable;
+using FluentResults;
+using MediatR;
+using OblivionDrive.Application.BillingPlanModule.DTOs;
+
+namespace OblivionDrive.Application.BillingPlanModule.Querys;
+public record GetAllBillingPlanQuery(int? Quantity) : IRequest<Result<BillingPlanResult>>;
+
+public record BillingPlanResult(ImmutableList<DetailBillingPlanDTO> BillingPlans);
---
+++ server/Application/BillingPlanModule/Querys/GetBillingPlanByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using FluentResults;
+using MediatR;
+using OblivionDrive.Application.BillingPlanModule.DTOs;
+
+namespace OblivionDrive.Application.BillingPlanModule.Querys;
+public record GetBillingPlanByIdQuery(Guid BillingPlanId) : IRequest<Result<DetailBillingPlanDTO>>;
