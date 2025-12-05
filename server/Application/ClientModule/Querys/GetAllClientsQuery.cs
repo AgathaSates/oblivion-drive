d491d56fc24c42dd959c104958bd80fe3049b453
Agatha Sates <satesagatha@gmail.com>
2025-12-05 17:01:28 -0300

    feat(application): add Client queries

---
+++ server/Application/ClientModule/Querys/GetAllClientsQuery.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Immutable;
+using FluentResults;
+using MediatR;
+using OblivionDrive.Application.ClientModule.DTOs;
+
+namespace OblivionDrive.Application.ClientModule.Querys;
+public record GetAllClientsQuery(int? Quantity) : IRequest<Result<ClientsResult>>;
+
+public record ClientsResult(ImmutableList<DetailClientDTO> Clients);
---
+++ server/Application/ClientModule/Querys/GetClientByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using FluentResults;
+using MediatR;
+using OblivionDrive.Application.ClientModule.DTOs;
+
+namespace OblivionDrive.Application.ClientModule.Querys;
+public record GetClientByIdQuery(Guid ClientId) : IRequest<Result<DetailClientDTO>>;
