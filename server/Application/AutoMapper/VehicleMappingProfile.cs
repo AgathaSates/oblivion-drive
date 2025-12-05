09181dacce8eb06226466fc89ea4706335fb4120
Agatha Sates <satesagatha@gmail.com>
2025-12-05 01:09:18 -0300

    feat(mapping): add AutoMapper profile for Vehicle

---
+++ server/Application/AutoMapper/VehicleMappingProfile.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using OblivionDrive.Application.VehicleModule.DTOs;
+using OblivionDrive.Domain.VehicleModule;
+
+namespace OblivionDrive.Application.AutoMapper;
+
+public class VehicleMappingProfile : Profile
+{
+    public VehicleMappingProfile()
+    {
+        CreateMap<Vehicle, VehicleDTO>()
+            .ConstructUsing(vehicle => new VehicleDTO(
+                true,
+                vehicle.LicensePlate,
+                vehicle.Brand,
+                vehicle.Model,
+                vehicle.Color,
+                vehicle.FuelType,
+                vehicle.FuelTankCapacityInLiters,
+                vehicle.Year,
+                vehicle.VehicleGroupId,
+                vehicle.PhotoBytes ?? Array.Empty<byte>()));
+
+        CreateMap<Vehicle, UpdatedVehicleDTO>()
+            .ConstructUsing(vehicle => new UpdatedVehicleDTO(
+                true,
+                vehicle.LicensePlate,
+                vehicle.Brand,
+                vehicle.Model,
+                vehicle.Color,
+                vehicle.FuelType,
+                vehicle.FuelTankCapacityInLiters,
+                vehicle.Year,
+                vehicle.VehicleGroupId,
+                vehicle.PhotoBytes ?? Array.Empty<byte>()));
+
+        CreateMap<Vehicle, DetailVehicleDTO>()
+            .ConstructUsing(vehicle => new DetailVehicleDTO(
+                vehicle.Id,
+                vehicle.LicensePlate,
+                vehicle.Brand,
+                vehicle.Model,
+                vehicle.Color,
+                vehicle.FuelType,
+                vehicle.FuelTankCapacityInLiters,
+                vehicle.Year,
+                vehicle.VehicleGroupId,
+                vehicle.PhotoBytes ?? Array.Empty<byte>()));
+    }
+}
