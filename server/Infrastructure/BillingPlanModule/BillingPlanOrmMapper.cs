8d47b73b83b41c12f95c9256f932f915bb996747
Agatha Sates <satesagatha@gmail.com>
2025-12-05 01:08:10 -0300

    feat(infrastructure): add BillingPlan infrastructure layer

---
+++ server/Infrastructure/BillingPlanModule/BillingPlanOrmMapper.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OblivionDrive.Domain.BillingPlanModule;
+
+namespace OblivionDrive.Infrastructure.Orm.BillingPlanModule;
+public class BillingPlanOrmMapper : IEntityTypeConfiguration<BillingPlan>
+{
+    public void Configure(EntityTypeBuilder<BillingPlan> builder)
+    {
+        builder.HasKey(bp => bp.Id);
+
+        builder.Property(bp => bp.CompanyId)
+                .IsRequired();
+
+        builder.Property(bp => bp.Name)
+               .IsRequired()
+               .HasMaxLength(200);
+
+        builder.HasOne(bp => bp.VehicleGroup)
+            .WithMany(vg => vg.BillingPlans)
+            .HasForeignKey(bp => bp.VehicleGroupId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.OwnsOne(bp => bp.DailyPlan, daily =>
+        {
+            daily.Property(p => p.DailyRate)
+                .HasColumnName("DailyPlan_DailyRate")
+                .HasColumnType("decimal(18,2)");
+
+            daily.Property(p => p.PricePerKilometer)
+                .HasColumnName("DailyPlan_PricePerKilometer")
+                .HasColumnType("decimal(18,2)");
+
+            daily.WithOwner();
+        });
+
+        builder.OwnsOne(bp => bp.ControlledPlan, controlled =>
+        {
+            controlled.Property(p => p.DailyRate)
+                .HasColumnName("ControlledPlan_DailyRate")
+                .HasColumnType("decimal(18,2)");
+
+            controlled.Property(p => p.ExtraPricePerKilometer)
+                .HasColumnName("ControlledPlan_ExtraPricePerKilometer")
+                .HasColumnType("decimal(18,2)");
+
+            controlled.WithOwner();
+        });
+
+        builder.OwnsOne(bp => bp.FreePlan, free =>
+        {
+            free.Property(p => p.DailyRate)
+                .HasColumnName("FreePlan_DailyRate")
+                .HasColumnType("decimal(18,2)");
+
+            free.WithOwner();
+        });
+
+        builder.Navigation(bp => bp.DailyPlan).IsRequired();
+        builder.Navigation(bp => bp.ControlledPlan).IsRequired();
+        builder.Navigation(bp => bp.FreePlan).IsRequired();
+    }
+}
---
+++ server/Infrastructure/BillingPlanModule/BillingPlanOrmRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OblivionDrive.Domain.BillingPlanModule;
+using OblivionDrive.Infrastructure.Orm.Shared;
+
+namespace OblivionDrive.Infrastructure.Orm.BillingPlanModule;
+public class BillingPlanOrmRepository(OblivionDriveDbContext context) : BaseRepository<BillingPlan>(context), IRepositoryBillingPlan
+{
+    public async Task<bool> ExistsByNameAsync(string vehicleGroupName)
+    {
+       return await context.BillingPlans.AnyAsync(plan => plan.Name == vehicleGroupName);
+    }
+
+    public async Task<bool> ExistsByNameAsync(string vehicleGroupName, Guid vehicleGroupIdToIgnore)
+    {
+        return await context.BillingPlans.AnyAsync(plan => plan.Name == vehicleGroupName && plan.Id != vehicleGroupIdToIgnore);
+    }
+
+    public async Task<bool> ExistsForVehicleGroupAsync(Guid vehicleGroupId, Guid billingPlanIdToIgnore)
+    {
+        return await context.BillingPlans
+        .AnyAsync(billingPlan =>
+            billingPlan.VehicleGroupId == vehicleGroupId && billingPlan.Id != billingPlanIdToIgnore);
+    }
+
+    public async Task<bool> ExistsForVehicleGroupAsync(Guid vehicleGroupId)
+    {
+        return await context.BillingPlans
+        .AnyAsync(billingPlan => billingPlan.VehicleGroupId == vehicleGroupId);
+    }
+}
