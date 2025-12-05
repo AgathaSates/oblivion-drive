06ff7dc296d9f4f8263fc30d67aa4887982b61f5
Agatha Sates <satesagatha@gmail.com>
2025-12-05 01:13:00 -0300

    feat(api): add AutoMapper profile for Vehicle models

---
+++ server/Api/AutoMapper/VehiclesModelsMappingProfile.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Immutable;
+using AutoMapper;
+using OblivionDrive.Api.Models.VehicleModule;
+using OblivionDrive.Application.VehicleModule.Commands;
+using OblivionDrive.Application.VehicleModule.DTOs;
+using OblivionDrive.Application.VehicleModule.Querys;
+
+namespace OblivionDrive.Api.AutoMapper;
+
+public sealed class VehiclesModelsMappingProfile : Profile
+{
+    public VehiclesModelsMappingProfile()
+    {
+        CreateMap<RegisterVehicleRequest, RegisterVehicleCommand>();
+        CreateMap<VehicleDTO, RegisterVehicleResponse>();
+        CreateMap<(Guid, UpdateVehicleRequest), UpdateVehicleCommand>()
+            .ConvertUsing(src => new UpdateVehicleCommand(
+                src.Item1,
+                src.Item2.Brand,
+                src.Item2.Model,
+                src.Item2.Color,
+                src.Item2.FuelType,
+                src.Item2.FuelTankCapacityInLiters,
+                src.Item2.Year,
+                src.Item2.VehicleGroupId,
+                src.Item2.PhotoBytes
+            ));
+
+        CreateMap<UpdatedVehicleDTO, UpdateVehicleResponse>();
+        CreateMap<DetailVehicleDTO, GetVehicleByIdResponse>();
+        CreateMap<VehiclesResult, GetAllVehiclesResponse>()
+            .ConvertUsing((src, dest, ctx) => new GetAllVehiclesResponse(
+                src.Vehicles.Count,
+                src?.Vehicles?
+                    .Select(v => ctx.Mapper.Map<DetailVehicleDTO>(v))
+                    .ToImmutableList() ?? ImmutableList<DetailVehicleDTO>.Empty
+            ));
+    }
+}
