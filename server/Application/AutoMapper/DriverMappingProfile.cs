1cfd6495e24b653bc1cfebd037365a478652ee6c
Agatha Sates <satesagatha@gmail.com>
2025-12-05 18:15:43 -0300

    feat(mapping): add AutoMapper profile for Driver

---
+++ server/Application/AutoMapper/DriverMappingProfile.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using OblivionDrive.Application.DriverModule.DTOs;
+using OblivionDrive.Domain.DriverModule;
+
+namespace OblivionDrive.Application.AutoMapper;
+public class DriverMappingProfile : Profile
+{
+    public DriverMappingProfile()
+    {
+        CreateMap<Driver, DriverDTO>()
+            .ConstructUsing(driver => new DriverDTO(
+                true,
+                driver.Name,
+                driver.Email,
+                driver.PhoneNumber,
+                driver.Cpf,
+                driver.Cnh,
+                driver.CnhExpirationDate,
+                driver.ClientId,
+                driver.IsClientAlsoDriver));
+
+        CreateMap<Driver, UpdatedDriverDTO>()
+            .ConstructUsing(driver => new UpdatedDriverDTO(
+                true,
+                driver.Name,
+                driver.Email,
+                driver.PhoneNumber,
+                driver.Cpf,
+                driver.Cnh,
+                driver.CnhExpirationDate,
+                driver.ClientId,
+                driver.IsClientAlsoDriver));
+
+        CreateMap<Driver, DetailDriverDTO>()
+            .ConstructUsing(driver => new DetailDriverDTO(
+                driver.Id,
+                driver.Name,
+                driver.Email,
+                driver.PhoneNumber,
+                driver.Cpf,
+                driver.Cnh,
+                driver.CnhExpirationDate,
+                driver.ClientId,
+                driver.IsClientAlsoDriver));
+    }
+}
