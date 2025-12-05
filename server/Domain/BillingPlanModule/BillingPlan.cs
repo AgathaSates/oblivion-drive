afe98dea3c233d67723d0bfa0f6e98dc65d766c7
Agatha Sates <satesagatha@gmail.com>
2025-12-05 01:07:02 -0300

    feat(domain): add BillingPlan domain model and repository interface

---
+++ server/Domain/BillingPlanModule/BillingPlan.cs
@@ -0,0 +1,89 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using OblivionDrive.Domain.Shared;
+using OblivionDrive.Domain.VehicleGroupModule;
+
+namespace OblivionDrive.Domain.BillingPlanModule;
+public class BillingPlan : TenantEntity<BillingPlan>
+{
+    public string Name { get; private set; }
+
+    public Guid VehicleGroupId { get; private set; }
+    public VehicleGroup VehicleGroup { get; private set; }
+
+    public DailyBillingPlanConfig DailyPlan { get; private set; }
+    public ControlledBillingPlanConfig ControlledPlan { get; private set; }
+    public FreeBillingPlanConfig FreePlan { get; private set; }
+
+    [ExcludeFromCodeCoverage]
+    private BillingPlan() { }
+
+    public BillingPlan(
+        string name,
+        Guid companyId,
+        Guid vehicleGroupId,
+        DailyBillingPlanConfig dailyPlan,
+        ControlledBillingPlanConfig controlledPlan,
+        FreeBillingPlanConfig freePlan)
+    {
+
+        Id = Guid.NewGuid();
+        CompanyId = companyId;
+        VehicleGroupId = vehicleGroupId;
+
+        Name = name;
+        DailyPlan = dailyPlan;
+        ControlledPlan = controlledPlan;
+        FreePlan = freePlan;
+    }
+
+    public override void Update(BillingPlan updatedEntity)
+    {
+        Name = updatedEntity.Name;
+        DailyPlan = updatedEntity.DailyPlan;
+        ControlledPlan = updatedEntity.ControlledPlan;
+        FreePlan = updatedEntity.FreePlan;
+    }
+}
+
+public class DailyBillingPlanConfig
+{
+    public decimal DailyRate { get; private set; }
+    public decimal PricePerKilometer { get; private set; }
+
+    [ExcludeFromCodeCoverage]
+    private DailyBillingPlanConfig() { }
+
+    public DailyBillingPlanConfig(decimal dailyRate, decimal pricePerKilometer)
+    {
+        DailyRate = dailyRate;
+        PricePerKilometer = pricePerKilometer;
+    }
+}
+
+public class ControlledBillingPlanConfig
+{
+    public decimal DailyRate { get; private set; }
+    public decimal ExtraPricePerKilometer { get; private set; }
+
+    [ExcludeFromCodeCoverage]
+    private ControlledBillingPlanConfig() { }
+
+    public ControlledBillingPlanConfig(decimal dailyRate, decimal extraPricePerKilometer)
+    {
+        DailyRate = dailyRate;
+        ExtraPricePerKilometer = extraPricePerKilometer;
+    }
+}
+
+public class FreeBillingPlanConfig
+{
+    public decimal DailyRate { get; private set; }
+
+    [ExcludeFromCodeCoverage]
+    private FreeBillingPlanConfig() { }
+
+    public FreeBillingPlanConfig(decimal dailyRate)
+    {
+        DailyRate = dailyRate;
+    }
+}
---
+++ server/Domain/BillingPlanModule/IRepositoryBillingPlan.cs
@@ -0,0 +1,10 @@
+﻿using OblivionDrive.Domain.Shared;
+
+namespace OblivionDrive.Domain.BillingPlanModule;
+public interface IRepositoryBillingPlan : IRepository<BillingPlan>
+{
+    Task<bool> ExistsForVehicleGroupAsync(Guid vehicleGroupId, Guid billingPlanIdToIgnore);
+    Task<bool> ExistsForVehicleGroupAsync(Guid vehicleGroupId);
+    Task<bool> ExistsByNameAsync(string vehicleGroupName);
+    Task<bool> ExistsByNameAsync(string vehicleGroupName, Guid vehicleGroupIdToIgnore);
+}
