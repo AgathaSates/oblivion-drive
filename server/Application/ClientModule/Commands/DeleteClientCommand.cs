5490a73b06ba35ddef96c4615b1defeeed9f5d48
Agatha Sates <satesagatha@gmail.com>
2025-12-05 17:01:14 -0300

    feat(application): add Client commands

---
+++ server/Application/ClientModule/Commands/DeleteClientCommand.cs
@@ -0,0 +1,5 @@
+﻿using FluentResults;
+using MediatR;
+
+namespace OblivionDrive.Application.ClientModule.Commands;
+public record DeleteClientCommand(Guid ClientId) : IRequest<Result>;
---
+++ server/Application/ClientModule/Commands/UpdateClientCommand.cs
@@ -0,0 +1,22 @@
+﻿using FluentResults;
+using MediatR;
+using OblivionDrive.Application.ClientModule.DTOs;
+using OblivionDrive.Domain.ClientModule;
+
+namespace OblivionDrive.Application.ClientModule.Commands;
+public record UpdateClientCommand(
+    Guid ClientId,
+    string Name,
+    string Email,
+    string PhoneNumber,
+    ClientType ClientType,
+    string? Cpf,
+    string? Rg,
+    string? Cnh,
+    string? Cnpj,
+    string State,
+    string City,
+    string District,
+    string Street,
+    string Number
+) : IRequest<Result<UpdatedClientDTO>>;
