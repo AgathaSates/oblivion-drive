78fd542ccc48c12bb1e9dba54861592ae403aab2
Agatha Sates <satesagatha@gmail.com>
2025-12-05 01:09:35 -0300

    feat(mapping): add AutoMapper profile for BillingPlan

---
+++ server/Application/AutoMapper/BillingPlanMappingProfile.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using OblivionDrive.Application.BillingPlanModule.DTOs;
+using OblivionDrive.Domain.BillingPlanModule;
+
+namespace OblivionDrive.Application.AutoMapper;
+public class BillingPlanMappingProfile : Profile
+{
+    public BillingPlanMappingProfile()
+    {
+        CreateMap<BillingPlan, BillingPlanDTO>()
+            .ConstructUsing(billingPlan => new BillingPlanDTO(
+                true,
+                billingPlan.Name,
+                billingPlan.VehicleGroupId,
+                billingPlan.DailyPlan.DailyRate,
+                billingPlan.DailyPlan.PricePerKilometer,
+                billingPlan.ControlledPlan.DailyRate,
+                billingPlan.ControlledPlan.ExtraPricePerKilometer,
+                billingPlan.FreePlan.DailyRate));
+
+        CreateMap<BillingPlan, UpdatedBillingPlanDTO>()
+            .ConstructUsing(billingPlan => new UpdatedBillingPlanDTO(
+                true,
+                billingPlan.Name,
+                billingPlan.VehicleGroupId,
+                billingPlan.DailyPlan.DailyRate,
+                billingPlan.DailyPlan.PricePerKilometer,
+                billingPlan.ControlledPlan.DailyRate,
+                billingPlan.ControlledPlan.ExtraPricePerKilometer,
+                billingPlan.FreePlan.DailyRate));
+
+        CreateMap<BillingPlan, DetailBillingPlanDTO>()
+            .ConstructUsing(billingPlan => new DetailBillingPlanDTO(
+                billingPlan.Id,
+                billingPlan.Name,
+                billingPlan.VehicleGroupId,
+                billingPlan.DailyPlan.DailyRate,
+                billingPlan.DailyPlan.PricePerKilometer,
+                billingPlan.ControlledPlan.DailyRate,
+                billingPlan.ControlledPlan.ExtraPricePerKilometer,
+                billingPlan.FreePlan.DailyRate));
+    }
+}
