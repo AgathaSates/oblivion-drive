da3694cf4e6afe4b48c18a1e1c3ac0cea9eb5f65
Agatha Sates <satesagatha@gmail.com>
2025-12-05 17:00:50 -0300

    feat(mapping): add AutoMapper profile for Client

---
+++ server/Application/AutoMapper/ClientMappingProfile.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using OblivionDrive.Application.ClientModule.DTOs;
+using OblivionDrive.Domain.ClientModule;
+
+namespace OblivionDrive.Application.AutoMapper;
+public class ClientMappingProfile : Profile
+{
+    public ClientMappingProfile()
+    {
+        CreateMap<Client, ClientDTO>()
+            .ConstructUsing(client => new ClientDTO(
+                true,
+                client.Name,
+                client.Email,
+                client.PhoneNumber,
+                client.ClientType,
+                client.Cpf,
+                client.Rg,
+                client.Cnh,
+                client.Cnpj,
+                client.Address.State,
+                client.Address.City,
+                client.Address.District,
+                client.Address.Street,
+                client.Address.Number));
+
+        CreateMap<Client, UpdatedClientDTO>()
+            .ConstructUsing(client => new UpdatedClientDTO(
+                true,
+                client.Name,
+                client.Email,
+                client.PhoneNumber,
+                client.ClientType,
+                client.Cpf,
+                client.Rg,
+                client.Cnh,
+                client.Cnpj,
+                client.Address.State,
+                client.Address.City,
+                client.Address.District,
+                client.Address.Street,
+                client.Address.Number));
+
+        CreateMap<Client, DetailClientDTO>()
+            .ConstructUsing(client => new DetailClientDTO(
+                client.Id,
+                client.Name,
+                client.Email,
+                client.PhoneNumber,
+                client.ClientType,
+                client.Cpf,
+                client.Rg,
+                client.Cnh,
+                client.Cnpj,
+                client.Address.State,
+                client.Address.City,
+                client.Address.District,
+                client.Address.Street,
+                client.Address.Number));
+    }
+}
