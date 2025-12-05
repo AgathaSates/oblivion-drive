fa32d2b710c834b185a45aa62bdc6f9fa2f60f94
Agatha Sates <satesagatha@gmail.com>
2025-12-05 01:06:35 -0300

    feat(domain): add Vehicle domain model and repository interface

---
+++ server/Domain/VehicleModule/IRepositoryVehicle.cs
@@ -0,0 +1,8 @@
+﻿using OblivionDrive.Domain.Shared;
+
+namespace OblivionDrive.Domain.VehicleModule;
+public interface IRepositoryVehicle : IRepository<Vehicle>
+{
+    Task AddPhotoAsync(Guid vehicleId, byte[] photoByte);
+    Task<List<Vehicle>> GetByVehicleGroupAsync(Guid vehicleGroupId);
+}
---
+++ server/Domain/VehicleModule/Vehicle.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using OblivionDrive.Domain.FuelPriceConfigurationModule;
+using OblivionDrive.Domain.Shared;
+using OblivionDrive.Domain.VehicleGroupModule;
+
+namespace OblivionDrive.Domain.VehicleModule;
+public class Vehicle : TenantEntity<Vehicle>
+{
+    public string LicensePlate { get; private set; }
+    public string Brand { get; private set; }
+    public string Model { get; private set; }
+    public string Color { get; private set; }
+
+    public FuelType FuelType { get; private set; }
+    public decimal FuelTankCapacityInLiters { get; private set; }
+    public int Year { get; private set; }
+
+    public Guid VehicleGroupId { get; private set; }
+    public VehicleGroup VehicleGroup { get; private set; } = null!;
+
+    public byte[]? PhotoBytes { get; private set; }
+
+    [ExcludeFromCodeCoverage]
+    private Vehicle() { }
+
+    public Vehicle(
+        string licensePlate, string brand, string model, string color, FuelType fuelType,
+        decimal fuelTankCapacityInLiters, int year, Guid vehicleGroupId, Guid companyId)
+    {
+        Id = Guid.NewGuid();
+        CompanyId = companyId;
+
+        LicensePlate = licensePlate;
+        Brand = brand;
+        Model = model;
+        Color = color;
+        FuelType = fuelType;
+        FuelTankCapacityInLiters = fuelTankCapacityInLiters;
+        Year = year;
+        VehicleGroupId = vehicleGroupId;
+    }
+
+    public override void Update(Vehicle updatedEntity)
+    {
+        Brand = updatedEntity.Brand;
+        Model = updatedEntity.Model;
+        Color = updatedEntity.Color;
+        FuelType = updatedEntity.FuelType;
+        FuelTankCapacityInLiters = updatedEntity.FuelTankCapacityInLiters;
+        Year = updatedEntity.Year;
+        VehicleGroupId = updatedEntity.VehicleGroupId;
+
+        PhotoBytes = updatedEntity.PhotoBytes;
+    }
+
+    public void SetPhoto(byte[] photoBytes)
+    {
+        PhotoBytes = photoBytes;
+    }
+}
