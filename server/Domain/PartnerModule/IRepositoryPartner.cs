b81f6e46219d5acc55a35c216cc78ef5b27df1c4
Agatha Sates <satesagatha@gmail.com>
2025-12-05 22:24:44 -0300

    feat(domain): add Partner domain model and repository interface

---
+++ server/Domain/PartnerModule/IRepositoryPartner.cs
@@ -0,0 +1,8 @@
+﻿using OblivionDrive.Domain.Shared;
+
+namespace OblivionDrive.Domain.PartnerModule;
+public interface IRepositoryPartner : IRepository<Partner>
+{
+    Task<bool> ExistsByNameAsync(string partnerName);
+    Task<bool> ExistsByNameAsync(string partnerName, Guid partnerIdToIgnore);
+}
---
+++ server/Domain/PartnerModule/Partner.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using OblivionDrive.Domain.CouponModule;
+using OblivionDrive.Domain.Shared;
+
+namespace OblivionDrive.Domain.PartnerModule;
+public class Partner : TenantEntity<Partner>
+{
+    public string Name { get; private set; }
+
+    public ICollection<Coupon> Coupons { get; private set; } = new List<Coupon>();
+
+    [ExcludeFromCodeCoverage]
+    private Partner() { }
+
+    public Partner(string name, Guid companyId)
+    {
+        Id = Guid.NewGuid();
+        CompanyId = companyId;
+
+        Name = name;
+    }
+
+    public override void Update(Partner updatedEntity)
+    {
+        Name = updatedEntity.Name;
+    }
+}
