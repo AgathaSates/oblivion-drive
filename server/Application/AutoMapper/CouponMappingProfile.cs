0df7fdd4acef37b668be9f8da8625e45e4ba7439
Agatha Sates <satesagatha@gmail.com>
2025-12-05 22:26:49 -0300

    feat(mapping): add AutoMapper profile for Coupon

---
+++ server/Application/AutoMapper/CouponMappingProfile.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using OblivionDrive.Application.CouponModule.DTOs;
+using OblivionDrive.Domain.CouponModule;
+
+namespace OblivionDrive.Application.AutoMapper;
+public class CouponMappingProfile : Profile
+{
+    public CouponMappingProfile()
+    {
+        CreateMap<Coupon, CouponDTO>()
+            .ConstructUsing(coupon => new CouponDTO(
+                true,
+                coupon.Name,
+                coupon.Value,
+                coupon.ExpirationDate,
+                coupon.PartnerId
+            ));
+
+        CreateMap<Coupon, UpdatedCouponDTO>()
+            .ConstructUsing(coupon => new UpdatedCouponDTO(
+                true,
+                coupon.Name,
+                coupon.Value,
+                coupon.ExpirationDate,
+                coupon.PartnerId
+            ));
+
+        CreateMap<Coupon, DetailCouponDTO>()
+            .ConstructUsing(coupon => new DetailCouponDTO(
+                coupon.Id,
+                coupon.Name,
+                coupon.Value,
+                coupon.ExpirationDate,
+                coupon.PartnerId
+            ));
+    }
+}
