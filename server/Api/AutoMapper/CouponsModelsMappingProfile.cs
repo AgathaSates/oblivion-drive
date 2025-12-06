10d49bb13ab9c3163234c57f1ca885895118d2ae
Agatha Sates <satesagatha@gmail.com>
2025-12-05 22:30:32 -0300

    feat(api): add AutoMapper profile for Coupon models

---
+++ server/Api/AutoMapper/CouponsModelsMappingProfile.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Immutable;
+using AutoMapper;
+using OblivionDrive.Api.Models.CouponModule.Requests;
+using OblivionDrive.Api.Models.CouponModule.Responses;
+using OblivionDrive.Application.CouponModule.Commands;
+using OblivionDrive.Application.CouponModule.DTOs;
+using OblivionDrive.Application.CouponModule.Querys;
+
+namespace OblivionDrive.Api.AutoMapper;
+
+public sealed class CouponsModelsMappingProfile : Profile
+{
+    public CouponsModelsMappingProfile()
+    {
+        CreateMap<RegisterCouponRequest, RegisterCouponCommand>();
+        CreateMap<(Guid, UpdateCouponRequest), UpdateCouponCommand>()
+            .ConvertUsing(src => new UpdateCouponCommand(
+                src.Item1,
+                src.Item2.Name,
+                src.Item2.Value,
+                src.Item2.ExpirationDate,
+                src.Item2.PartnerId
+            ));
+
+        CreateMap<CouponDTO, RegisterCouponResponse>();
+        CreateMap<UpdatedCouponDTO, UpdateCouponResponse>();
+        CreateMap<DetailCouponDTO, GetCouponByIdResponse>();
+        CreateMap<CouponsResult, GetAllCouponsResponse>()
+            .ConvertUsing((src, dest, ctx) => new GetAllCouponsResponse(
+                Quantity: src.Coupons.Count,
+                Coupons: src?.Coupons?
+                    .Select(c => ctx.Mapper.Map<DetailCouponDTO>(c))
+                    .ToImmutableList() ?? ImmutableList<DetailCouponDTO>.Empty
+            ));
+    }
+}
