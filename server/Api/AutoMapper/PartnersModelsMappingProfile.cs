0369473bfb18f36931dd2c532bcae10135d79e2b
Agatha Sates <satesagatha@gmail.com>
2025-12-05 22:30:23 -0300

    feat(api): add AutoMapper profile for Partner models

---
+++ server/Api/AutoMapper/PartnersModelsMappingProfile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Immutable;
+using AutoMapper;
+using OblivionDrive.Api.Models.PartnerModule.Requests;
+using OblivionDrive.Api.Models.PartnerModule.Responses;
+using OblivionDrive.Application.PartnerModule.Commands;
+using OblivionDrive.Application.PartnerModule.DTOs;
+using OblivionDrive.Application.PartnerModule.Querys;
+
+namespace OblivionDrive.Api.AutoMapper;
+
+public sealed class PartnersModelsMappingProfile : Profile
+{
+    public PartnersModelsMappingProfile()
+    {
+        CreateMap<RegisterPartnerRequest, RegisterPartnerCommand>();
+        CreateMap<(Guid, UpdatePartnerRequest), UpdatePartnerCommand>()
+            .ConvertUsing(src => new UpdatePartnerCommand(
+                src.Item1,
+                src.Item2.Name
+            ));
+
+        CreateMap<PartnerDTO, RegisterPartnerResponse>();
+        CreateMap<UpdatedPartnerDTO, UpdatePartnerResponse>();
+        CreateMap<DetailPartnerDTO, GetPartnerByIdResponse>();
+        CreateMap<PartnersResult, GetAllPartnersResponse>()
+            .ConvertUsing((src, dest, ctx) => new GetAllPartnersResponse(
+                Quantity: src.Partners.Count,
+                Partners: src?.Partners?
+                    .Select(p => ctx.Mapper.Map<DetailPartnerDTO>(p))
+                    .ToImmutableList() ?? ImmutableList<DetailPartnerDTO>.Empty
+            ));
+    }
+}
