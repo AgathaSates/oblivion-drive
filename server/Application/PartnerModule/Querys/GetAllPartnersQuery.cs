30e87b00571cdf5160aeb2f1079bc3a91b55bcc3
Agatha Sates <satesagatha@gmail.com>
2025-12-05 22:28:59 -0300

    feat(application): add Partner queries

---
+++ server/Application/PartnerModule/Querys/GetAllPartnersQuery.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Immutable;
+using FluentResults;
+using MediatR;
+using OblivionDrive.Application.PartnerModule.DTOs;
+
+namespace OblivionDrive.Application.PartnerModule.Querys;
+public record GetAllPartnersQuery(int? Quantity) : IRequest<Result<PartnersResult>>;
+
+public record PartnersResult(ImmutableList<DetailPartnerDTO> Partners);
---
+++ server/Application/PartnerModule/Querys/GetPartnerByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using FluentResults;
+using MediatR;
+using OblivionDrive.Application.PartnerModule.DTOs;
+
+namespace OblivionDrive.Application.PartnerModule.Querys;
+public record GetPartnerByIdQuery(Guid PartnerId) : IRequest<Result<DetailPartnerDTO>>;
