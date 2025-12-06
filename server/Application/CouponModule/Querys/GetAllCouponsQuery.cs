44e6fa5e2293f839f8292e8b5075a090bf4eaf82
Agatha Sates <satesagatha@gmail.com>
2025-12-05 22:29:10 -0300

    feat(application): add Coupon queries

---
+++ server/Application/CouponModule/Querys/GetAllCouponsQuery.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Immutable;
+using FluentResults;
+using MediatR;
+using OblivionDrive.Application.CouponModule.DTOs;
+
+namespace OblivionDrive.Application.CouponModule.Querys;
+public record GetAllCouponsQuery(int? Quantity) : IRequest<Result<CouponsResult>>;
+
+public record CouponsResult(ImmutableList<DetailCouponDTO> Coupons);
---
+++ server/Application/CouponModule/Querys/GetCouponByIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using FluentResults;
+using MediatR;
+using OblivionDrive.Application.CouponModule.DTOs;
+
+namespace OblivionDrive.Application.CouponModule.Querys;
+
+public record GetCouponByIdQuery(Guid CouponId) : IRequest<Result<DetailCouponDTO>>;
