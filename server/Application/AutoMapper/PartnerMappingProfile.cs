8eb66818a76af4d4b94e9418c14acd86b66bdfec
Agatha Sates <satesagatha@gmail.com>
2025-12-05 22:26:40 -0300

    feat(mapping): add AutoMapper profile for Partner

---
+++ server/Application/AutoMapper/PartnerMappingProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using OblivionDrive.Application.PartnerModule.DTOs;
+using OblivionDrive.Domain.PartnerModule;
+
+namespace OblivionDrive.Application.AutoMapper;
+public class PartnerMappingProfile : Profile
+{
+    public PartnerMappingProfile()
+    {
+        CreateMap<Partner, PartnerDTO>()
+            .ConstructUsing(partner => new PartnerDTO(
+                true,
+                partner.Name
+            ));
+
+        CreateMap<Partner, UpdatedPartnerDTO>()
+            .ConstructUsing(partner => new UpdatedPartnerDTO(
+                true,
+                partner.Name
+            ));
+
+        CreateMap<Partner, DetailPartnerDTO>()
+            .ConstructUsing(partner => new DetailPartnerDTO(
+                partner.Id,
+                partner.Name
+            ));
+    }
+}
