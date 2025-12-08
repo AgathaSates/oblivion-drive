11591c5001304d64a8d377eb7a7431083c05435a
Agatha Sates <satesagatha@gmail.com>
2025-12-08 02:56:16 -0300

    feat(application): add Rental queries

---
+++ server/Application/RentalModule/Querys/GetAllRentalsQuery.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Immutable;
+using FluentResults;
+using MediatR;
+using OblivionDrive.Application.RentalModule.DTOs;
+
+namespace OblivionDrive.Application.RentalModule.Querys;
+public record GetAllRentalsQuery(int? Quantity) : IRequest<Result<RentalsResult>>;
+
+public record RentalsResult(ImmutableList<DetailRentalDTO> Rentals);
---
+++ server/Application/RentalModule/Querys/GetRentalByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using FluentResults;
+using MediatR;
+using OblivionDrive.Application.RentalModule.DTOs;
+
+namespace OblivionDrive.Application.RentalModule.Querys;
+public record GetRentalByIdQuery(Guid RentalId) : IRequest<Result<DetailRentalDTO>>;
