460e3157f0e72403b76b167f60f48fb989c4281d
Agatha Sates <satesagatha@gmail.com>
2025-12-08 02:56:55 -0300

    feat(mapping): add AutoMapper profile for Rental

---
+++ server/Application/AutoMapper/RentalMappingProfile.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using OblivionDrive.Application.RentalModule.DTOs;
+using OblivionDrive.Domain.RentalModule;
+
+namespace OblivionDrive.Application.AutoMapper;
+public class RentalMappingProfile : Profile
+{
+    public RentalMappingProfile()
+    {
+        CreateMap<Rental, RentalDTO>()
+            .ConstructUsing(rental => new RentalDTO(
+                true,
+                rental.Id,
+                rental.EstimatedRentalAmount));
+
+        CreateMap<Rental, UpdatedRentalDTO>()
+            .ConstructUsing(rental => new UpdatedRentalDTO(
+                true,
+                rental.Id,
+                rental.EstimatedRentalAmount));
+
+        CreateMap<Rental, CompletedRentalDTO>()
+            .ConstructUsing(rental => new CompletedRentalDTO(
+                true,
+                rental.Id,
+                rental.GrossRentalAmount,
+                rental.FinalAmountToPay,
+                rental.CouponId,
+                rental.CouponDiscountAmount));
+
+        CreateMap<Rental, DetailRentalDTO>()
+            .ConstructUsing(rental => new DetailRentalDTO(
+                rental.Id,
+                rental.ClientId,
+                rental.DriverId,
+                rental.VehicleId,
+                rental.PlanType,
+                rental.StartDate,
+                rental.ExpectedReturnDate,
+                rental.ActualReturnDate,
+                rental.EstimatedRentalAmount,
+                rental.GrossRentalAmount,
+                rental.FinalAmountToPay,
+                rental.IsCompleted,
+                rental.CouponId,
+                rental.ServiceIds));
+    }
+}
